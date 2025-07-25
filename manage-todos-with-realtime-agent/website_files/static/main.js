a1ef4622d3f9f22f8be7300cd36243f135d8ac2d
chethasp <65081481+chethasp@users.noreply.github.com>
2025-07-25 15:53:18 -0400

    Delete manage-todos-with-realtime-agent directory

--- manage-todos-with-realtime-agent/website_files/static/main.js
+++
@@ -1,6 +0,0 @@
-import { Audio } from './Audio.js';
-
-// Create an instance of AudioPlayer with the WebSocket URL
-const audio = new Audio(socketUrl);
-// Start receiving and playing audio
-audio.start();
